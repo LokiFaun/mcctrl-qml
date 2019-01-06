36c96c045f0d595692c79ea664b0f2ae557047e2
LokiFaun <johannes.schuetz@outlook.com>
2019-01-06 14:56:24 +0100

    improve toolchain file

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 mkdir build
 cd build
-export CC=/home/schuetz/raspberry_pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc 
-export CXX=/home/schuetz/raspberry_pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++ 
-cmake .. -DWITH_TLS=OFF -DWITH_SRV=OFF -DWITH_THREADING=OFF
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../rpi.cmake
 make -j
--- rpi.cmake
+++ rpi.cmake
@@ -1,9 +1,10 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_SYSROOT /home/schuetz/raspberry_pi/sysroot)
-set(CMAKE_STAGING_PREFIX /home/schuetz/stage)
+#set(CMAKE_SYSROOT /home/schuetz/raspberry_pi/sysroot)
+#set(CMAKE_STAGING_PREFIX /home/schuetz/stage)
 
+set(tools /home/schuetz/raspberry_pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
@@ -11,5 +12,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(WITH_TLS OFF)
+set(WITH_SRV OFF)
+set(WITH_THREADING OFF)
 
 message(STATUS "using Raspberry Pi toolchain")
