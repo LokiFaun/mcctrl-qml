897f0b79b1d4ef1ed8cac845a9498fb6abf2322c
LokiFaun <johannes.schuetz@outlook.com>
2019-01-06 15:35:55 +0100

    improve build system

--- rpi.cmake
+++ rpi.cmake
@@ -1,12 +1,12 @@
-set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 #set(CMAKE_SYSROOT /home/schuetz/raspberry_pi/sysroot)
 #set(CMAKE_STAGING_PREFIX /home/schuetz/stage)
 
-set(tools /home/schuetz/raspberry_pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/)
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(tools /home/schuetz/raspberry_pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin)
+set(CMAKE_C_COMPILER ${tools}/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/arm-linux-gnueabihf-g++)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
